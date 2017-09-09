1769fb59855f4ffe9fa70e02342754b4e59f4732
aimanabdou <22576695+aimanabdou@users.noreply.github.com>
2017-09-09 20:53:23 +0200

    Change Log - Added a Settings class for TimeAgo.

---
+++ src/Time/TimeAgoSettings.java
@@ -0,0 +1,12 @@
+package Time;
+
+public class TimeAgoSettings {
+
+	final public static boolean showYears	= true;
+	final public static boolean showMonths	= true;
+	final public static boolean showWeeks	= true;
+	final public static boolean showDays	= true;
+	final public static boolean showHours	= true;
+	final public static boolean showMinutes	= true;
+	final public static boolean showSeconds = true;
+}
